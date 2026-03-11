93c98164fe6c54ee76133556117e6a144d0197a9
Carlos Romero <carlosromeros779@gmail.com>
2026-03-11 13:54:13 -0400

    primer commit

---
+++ animal.py
@@ -0,0 +1,26 @@
+class Animal:
+    def __init__(self,nombre, edad,statura):
+        self.nombre = nombre
+        self.edad = edad
+        self.statura = statura
+
+    def hacer_sonido(self,nombre):
+        diccionario_sonidos = {
+            "Perro": "Guau",
+            "Gato": "Miau",
+            "Vaca": "Muu"}
+        
+        if nombre in diccionario_sonidos:
+            return diccionario_sonidos[nombre]
+        else:   
+            return "Sonido desconocido"
+
+
+
+
+animal = Animal("Perro", 5, "Grande")
+print("Nombre:", animal.nombre)
+print("Edad:", animal.edad)
+print("Statura:", animal.statura)
+sonido = animal.hacer_sonido(animal.nombre)
+print("Sonido:", sonido)
